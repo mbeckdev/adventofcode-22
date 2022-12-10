914f27d086a72dd7a9d7e8c64768c3ee42e1f22f
mbeckdev <mbeckdev42@gmail.com>
2022-12-10 10:40:38 -0700

    Day 6 Puzzle 1 done

--- src/puzzles/Day6Puzzle1.jsx
+++ src/puzzles/Day6Puzzle1.jsx
@@ -25,12 +25,59 @@ function Day6Puzzle1() {
 
   function getAnswer() {
     // console.log('formattedData', formattedData);
-
+    // console.log(formattedData[0]);
+    // console.log(formattedData[1]);
+    // console.log(formattedData[2]);
+    // console.log(formattedData[3]);
     let answer = 3;
 
-    answer = 42;
+    //for every character in long string
+    // for (let i = 3; i < formattedData[i].length; i++) {
+    //   console.log('asdfasdf');
+    //   let last4 = formattedData.slice(i - 4, i);
+    //   console.log('last4', last4);
+    // }
+    // console.log('asdfasdfasdf');
+    // console.log('asdf.lengh', formattedData.length);
+    let firstMarkerIndex = returnFirstMarkerIndex(formattedData);
+    let indexInEnglish = firstMarkerIndex + 1;
+
+    function is4AllDifferent(fourString) {
+      // const is4AllDifferent => (fourString) => {
+      let values = new Map();
+
+      for (let i = 0; i < fourString.length; i++) {
+        // console.log('values map = ', values);
+        if (values.has(fourString[i])) {
+          return false;
+        }
+        values.set(fourString[i]);
+      }
+      return true;
+    }
+
+    function returnFirstMarkerIndex(formattedData) {
+      for (let i = 4 - 1; i < formattedData.length; i++) {
+        let last4 = formattedData.slice(i - 4 + 1, i + 1);
+        // console.log('last4', last4);
+        let last4AreDifferent = is4AllDifferent(last4);
+        if (last4AreDifferent) {
+          // console.log(
+          //   'last 4 were different, first marker index is ',
+          //   i,
+          //   ' and is ',
+          //   formattedData[i]
+          // );
+          return i;
+        }
+        // console.log('last4arediff', last4AreDifferent);
+      }
+      return -1;
+    }
+
+    answer = indexInEnglish;
     answer +=
-      ' = The number of Calories of the top three elves carrying the most calories. Is this going to turn into Hunger Games?';
+      ' = The place wher the first start-of-packet marker is complete after this many characters have been processed checking for 4 chars that are different. - Using some broken device to communicate with weird signals. The elves are on the move now!';
     return answer;
   }
 
@@ -42,25 +89,11 @@ function Day6Puzzle1() {
     // console.log('splitting up data');
     // , rawText =', rawText);
 
-    let arrayOfLines = rawText.split('\r\n');
+    // let arrayOfLines = rawText.split('\r\n');
+    let arrayOfLines = rawText;
     // console.log(arrayOfLines);
-    let tempElfArray: string = [];
-    let finalCalorieArray: Array = [];
 
-    // console.log('aaa arrayOfLines.length', arrayOfLines.length);
-    for (let i = 0; i < arrayOfLines.length; i++) {
-      if (arrayOfLines[i]) {
-        tempElfArray.push(Number(arrayOfLines[i]));
-      } else {
-        finalCalorieArray.push(tempElfArray);
-        tempElfArray = [];
-      }
-    }
-    finalCalorieArray.push(tempElfArray);
-    tempElfArray = [];
-    // console.log('finalCalorieArray', finalCalorieArray);
-
-    formattedData = finalCalorieArray;
+    formattedData = arrayOfLines;
   };
 
   // ************************
